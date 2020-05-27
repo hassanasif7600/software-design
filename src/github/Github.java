19d909ec21d09b8eb2356d05995231787847a359
hassanasif7600 <asif0@sheridan.desire2learn.com>
2020-05-27 17:01:19 -0400

    first commit

--- src/github/Github.java
+++ src/github/Github.java
@@ -15,7 +15,7 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("learn fam");
+        System.out.println("learn fam ok");
     }
     
 }
